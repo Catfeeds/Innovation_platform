7dbdbeca9fb648f287624348184607f731cfef9c
Ryan <HTB794409767@163.com>
2018-07-31 14:32:15 +0800

    Initial commit|Finish Config

---
+++ src/main/java/com/tech/common/TokenCache.java
@@ -0,0 +1,39 @@
+package com.tech.common;
+
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+import java.util.concurrent.TimeUnit;
+
+public class TokenCache {
+    public static final String TOKEN_PREFIX = "token_";
+
+    private static LoadingCache<String,String> loadingCache = CacheBuilder.newBuilder().initialCapacity(1000).maximumSize(10000).expireAfterAccess(5, TimeUnit.MINUTES)
+            .build(new CacheLoader<String, String>() {
+                @Override
+                public String load(String s) throws Exception {
+                    return "null";
+                }
+            });
+
+    public static void setKey(String key,String value){
+        loadingCache.put(key,value);
+    }
+
+    public static String getKey(String key){
+        String value = null;
+        try {
+            value = loadingCache.get(key);
+            if ("null".equals(value)){
+                return null;
+            }
+            return value;
+        } catch (Exception e){
+            //logger.error("localCache get error",e);
+        }
+        return null;
+    }
+
+}
